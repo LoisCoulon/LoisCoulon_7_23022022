b82c39b809793b8b8e06955107ce84400827ba4e
Loïs <flojuve@hotmail.fr>
2022-03-02 15:59:54 +0100

    native loop version

--- scripts/utils/FilterData.js
+++ scripts/utils/FilterData.js
@@ -8,13 +8,18 @@ class FilterData {
     this.searchBar.addEventListener("input", (e) => {
       let value = e.target.value.toLowerCase();
 
-      if(value.length >= 3) {
-        const filteredArray = this.recipes.filter(recipe => recipe.name.toLowerCase().includes(value))
-        new RecipeFactory(filteredArray).createRecipes()
+      if (value.length >= 3) {
+        const filteredArray = [];
+        for (let index = 0; index < this.recipes.length; index++) {
+          const recipe = this.recipes[index];
+          if (recipe.name.toLowerCase().includes(value)) {
+            filteredArray.push(recipe);
+          }
+        }
+        new RecipeFactory(filteredArray).createRecipes();
       } else {
-        new RecipeFactory(this.recipes).createRecipes()
+        new RecipeFactory(this.recipes).createRecipes();
       }
-    })
+    });
   }
 }
-   
