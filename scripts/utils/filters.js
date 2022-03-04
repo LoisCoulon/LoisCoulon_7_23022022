8e038134b5b3220e01748451b0ef3dd561fa0551
Loïs <flojuve@hotmail.fr>
2022-03-04 12:14:00 +0100

    filter style

--- scripts/utils/filters.js
+++ scripts/utils/filters.js
@@ -2,20 +2,30 @@ const selectIngredient = document.querySelector(".ingredients");
 const selectAppliances = document.querySelector(".appliances");
 const selectUstensils = document.querySelector(".ustensils");
 
+function openDropDown(element) {
+  const input = element.firstElementChild
+  const selector = element.className;
+  element.classList.add(selector + "-active");
+  input.setAttribute("placeHolder", input.ariaLabel) 
+}
+
+function closeDropDown(element) {
+  const input = element.firstElementChild
+  element.classList.remove(element.classList.item(1));
+  input.setAttribute("placeHolder", input.name) 
+}
+
 function dropdown(element) {
   element.addEventListener("click", function () {
-    const input = element.firstElementChild
 
     if (element.classList.length > 1) {
-      element.classList.remove(element.classList.item(1));
-      input.setAttribute("placeHolder", input.name) 
+      closeDropDown(element)
     } else {
-      const selector = element.className;
-      element.classList.add(selector + "-active");
-      input.setAttribute("placeHolder", input.ariaLabel) 
+      openDropDown(element)
     }
   });
 }
+
 dropdown(selectIngredient);
 dropdown(selectAppliances);
 dropdown(selectUstensils);
