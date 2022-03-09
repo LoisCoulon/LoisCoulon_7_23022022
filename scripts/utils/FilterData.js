ec74f6e4f90688306e37d21b8647b2b384f7245a
Loïs <flojuve@hotmail.fr>
2022-03-09 11:35:54 +0100

    filter appliances by tag

--- scripts/utils/FilterData.js
+++ scripts/utils/FilterData.js
@@ -10,6 +10,7 @@ class FilterData {
   init() {
     this.recipeFactory.createRecipes(this.recipes);
     this.itemFactory.createItems(this.recipes);
+    this.filterAppliances()
   }
 
   filterData() {
@@ -24,7 +25,47 @@ class FilterData {
         this.itemFactory.createItems(filteredArray);
       } else {
         this.recipeFactory.createRecipes(this.recipes);
+        this.itemFactory.createItems(this.recipes)
       }
     });
   }
+
+  filterIngredients() {
+    const ingredientInput = document.querySelector(".ingredients input")
+  }
+
+  filterAppliances() {
+    // recherche du tag
+    const applianceInput = document.querySelector(".appliances input")
+    applianceInput.addEventListener("input", (e) => {
+      let value = e.target.value.toLowerCase();
+      if (value.length > 0) {
+        const data = this.recipes.filter((recipe) =>
+          recipe.appliance.toLowerCase().includes(value))
+        console.log(data)
+        this.itemFactory.createItems(data);
+      } else {
+      this.recipeFactory.createRecipes(this.recipes);
+      this.itemFactory.createItems(this.recipes);
+      }
+    });
+
+    // filtrage des recettes par appareil au clic
+    document.querySelectorAll(".search-section .filters .appliances-list li").forEach(li => 
+      li.addEventListener("click", (e) => {
+        let value = e.target.dataset.set;
+        const data = this.recipes.filter((recipe) =>
+        recipe.appliance.toLowerCase().includes(value))
+        this.itemFactory.createItems(data);
+        this.recipeFactory.createRecipes(data);
+      })
+    )
+  }
+
+  filterUstensils() {
+    const ustensilInput = document.querySelector(".ustensils input")
+  }
+
+
+
 }
