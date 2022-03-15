e83a580f290c5fa6b74d68531039861774765650
Loïs <flojuve@hotmail.fr>
2022-03-15 11:54:22 +0100

    filter functions ok

--- scripts/utils/FilterData.js
+++ scripts/utils/FilterData.js
@@ -10,65 +10,120 @@ class FilterData {
   init() {
     this.recipeFactory.createRecipes(this.recipes);
     this.itemFactory.createItems(this.recipes);
-    this.filterIngredients()
-    this.filterUstensils(this.recipes)
-    this.filterAppliances(this.recipes)
-    this.filterApplianceOnClick(this.recipes)
-    this.filterUstensilOnClick(this.recipes)
+    // this.filterIngredients(this.recipes)
+    // this.filterIngredientsOnClick(this.recipes)
+    // this.filterUstensils(this.recipes)
+    // this.filterAppliances(this.recipes)
+    // this.filterApplianceOnClick(this.recipes)
+    // this.filterUstensilOnClick(this.recipes)
   }
 
   filterData() {
     this.searchBar.addEventListener("input", (e) => {
       let value = e.target.value.toLowerCase();
-      const filteredArray = this.recipes.filter((recipe) =>
+      let filteredArrayName = this.recipes.filter((recipe) =>
       recipe.name.toLowerCase().includes(value)
-    );
+      );
+      let filteredArrayDescription = this.recipes.filter((recipe)=> 
+      recipe.description.toLowerCase().includes(value)
+      );
+      let filteredArrayIngredient = this.recipes.filter((recipe) =>
+      recipe.ingredients.some((ingredient) => {
+       return ingredient.ingredient.toLowerCase().includes(value) 
+      })  
+      );
+
+      const res1 = filteredArrayDescription.filter((el1) => !filteredArrayName.find(el2 => el1.id === el2.id))
+      for(const result1 in res1) {
+        filteredArrayName.push(res1[result1])
+      }
+      const res2 = filteredArrayIngredient.filter((el1) => !filteredArrayName.find(el2 => el1.id === el2.id))
+      for(const result2 in res2) {
+        filteredArrayName.push(res2[result2])
+      }
+      const filteredResult = filteredArrayName 
+
       if (value.length >= 3) {
-        
-        this.recipeFactory.createRecipes(filteredArray);
-        this.itemFactory.createItems(filteredArray);
-        this.filterApplianceOnClick(filteredArray)
-        this.filterUstensilOnClick(filteredArray)
+        this.recipeFactory.createRecipes(filteredResult);
+        this.itemFactory.createItems(filteredResult);
+        this.filterApplianceOnClick(filteredResult)
+        this.filterUstensilOnClick(filteredResult)
       } else {
         this.recipeFactory.createRecipes(this.recipes);
         this.itemFactory.createItems(this.recipes)
       }
     });
-  }
-
-  filterIngredients() {
-    // On récupère le tableau des ingédients filtré (retourne seulement un tableau filtré)
-    const ingredientInput = document.querySelector(".ingredients input")
-    ingredientInput.addEventListener("input", (e) => {
-      let value = e.target.value.toLowerCase();
-
-      let filteredArray = []
-      this.recipes.filter((recipe) => {
-        recipe.ingredients.filter((ingredient) =>{
-          if(ingredient.ingredient.toLowerCase().includes(value)) {
-            filteredArray.push(ingredient.ingredient)
-          }
-        })
-      })   
-      console.log(filteredArray)
-    });
-  }
-
-  filterAppliances(recipes) {
-    // recherche du tag
+    //filtrage des tags des appareils
     const applianceInput = document.querySelector(".appliances input")
     applianceInput.addEventListener("input", (e) => {
-      let value = e.target.value.toLowerCase();
+      this.filterAppliances(this.recipes, e)
+    });
+    // filtrage des recettes par appareil
+    this.filterApplianceOnClick(this.recipes)
 
-      if (value.length > 0) {
-        const data = recipes.filter((recipe) =>
-        recipe.appliance.toLowerCase().includes(value))
-        this.itemFactory.createItems(data);
-        this.filterApplianceOnClick(data)
-      } else {
+    //filtrage des tags des ustensils
+    const ustensilInput = document.querySelector(".ustensils input")
+    ustensilInput.addEventListener("input", (e) => {
+      this.filterUstensils(this.recipes, e)
+    })
+    // filtrage des recettes par ustensil
+    this.filterUstensilOnClick(this.recipes)
+
+    //filtrage des tags des ingredients
+    const ingredientInput = document.querySelector(".ingredients input")
+    ingredientInput.addEventListener("input", (e) => {
+      this.filterIngredients(this.recipes, e)
+    });
+
+    // filtrage des recettes par ingredient
+    this.filterIngredientsOnClick(this.recipes)
+  }
+
+  filterIngredients(recipes, e) {
+    // On récupère le tableau des ingédients filtré (retourne seulement un tableau filtré)
+    let value = e.target.value.toLowerCase();
+    if (value.length > 0) {
+      const data = recipes.filter((recipe) =>
+        recipe.ingredients.some((ingredient) => {
+          return ingredient.ingredient.toLowerCase().includes(value) 
+        })  
+      )
+      this.itemFactory.createItems(data);
+      this.filterIngredientsOnClick(data);
+    } else {
       this.itemFactory.createItems(recipes);
-      }
-    });    
+    }
+  }
+
+  filterIngredientsOnClick(recipes) {
+    // filtrage des ingredients au clic
+    document.querySelectorAll(".search-section .filters .ingredients-list li").forEach(li => 
+      li.addEventListener("click", (e) => {
+        let value = e.target.dataset.set;
+        const data = recipes.filter((recipe) =>
+          recipe.ingredients.some((ingredient) => {
+            return ingredient.ingredient.toLowerCase().includes(value) 
+          })
+        ) 
+        this.itemFactory.createItems(data);
+        this.recipeFactory.createRecipes(data);
+      })
+    )
+    
+  }
+
+  filterAppliances(recipes, e) {
+    // recherche du tag
+    let value = e.target.value.toLowerCase();
+    console.log(value)
+    if (value.length > 0) {
+      const data = recipes.filter((recipe) =>
+      recipe.appliance.toLowerCase().includes(value))
+      this.itemFactory.createItems(data);
+      this.filterApplianceOnClick(data)
+    } else {
+    this.itemFactory.createItems(recipes);
+    }
   }
 
   filterApplianceOnClick(recipes) {
@@ -79,67 +134,43 @@ class FilterData {
         const data = recipes.filter((recipe) => 
         recipe.appliance.toLowerCase().includes(value)
         )
-        console.log(data)
         this.itemFactory.createItems(data);
         this.recipeFactory.createRecipes(data);
       })
     )
   }
 
-  filterUstensils(recipes) {
-    const ustensilInput = document.querySelector(".ustensils input")
-    let filteredArray = []
+  filterUstensils(recipes, e) {
+ 
+    let value = e.target.value.toLowerCase();
+    if (value.length > 0) {
 
-    ustensilInput.addEventListener("input", (e) => {
-      let value = e.target.value.toLowerCase();
-
-      recipes.map((recipe) => {
-        recipe.ustensils.forEach((ustensil) =>{
-          if(ustensil.toLowerCase().includes(value)) {
-            filteredArray.push(ustensil)
-          }
+      const data = recipes.filter((recipe) => 
+        recipe.ustensils.some(ustensil => {
+          return ustensil.toLowerCase().includes(value)
         })
-      })   
-      console.log(filteredArray)
-      if (value.length > 0) {
-
-        const data = recipes.filter((recipe) => 
-          filteredArray.every(ustensil => {
-            return recipe.ustensils.includes(ustensil)
-          })
-        )
-        filteredArray = []      
-        this.itemFactory.createItems(data);
-        this.filterUstensilOnClick(data)
-      } else {
-      this.itemFactory.createItems(recipes);
-      }
-    })
+      )
+      console.log(data)
+      this.itemFactory.createItems(data);
+      this.filterUstensilOnClick(data)
+    } else {
+    this.itemFactory.createItems(recipes);
+    }
   }
 
   filterUstensilOnClick(recipes) {
     document.querySelectorAll(".search-section .filters .ustensils-list li").forEach(li => 
       li.addEventListener("click", (e) => {
-        let filteredArray = []
         let value = e.target.dataset.set;
-        recipes.map((recipe) => {
-          recipe.ustensils.forEach((ustensil) =>{
-            if(ustensil.toLowerCase().includes(value)) {
-              filteredArray.push(ustensil)
-            }
-          })
-        })   
-        console.log(filteredArray)
-  
         const data = recipes.filter((recipe) => 
-          filteredArray.every(ustensil => {
-            return recipe.ustensils.includes(ustensil)
+          recipe.ustensils.some(ustensil => {
+            return ustensil.toLowerCase().includes(value)
           })
         )
-        filteredArray = []      
         this.itemFactory.createItems(data);
         this.recipeFactory.createRecipes(data);
       })
     )
+    
   }
 }
